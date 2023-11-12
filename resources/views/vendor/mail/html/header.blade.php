aca84e03730312764823f394b440b9b156c240ed
Jasper <j.stolwijk@flooris.nl>
2023-11-12 21:48:12 +0100

    Changed Favicon
    - Changed favicon to the new logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Crossfire Tournament')
-<img src="{{ asset('img/logo.png') }}" class="logo" alt="Laravel Logo">
+<img src="{{ asset('img/logo.png') }}" class="logo" alt="Crossfire Tournaments Logo">
 @else
 {{ $slot }}
 @endif
